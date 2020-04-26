99eaf6abb127dee82542bd13aeea7775c5abf4a8
Alexander Fleischer <fleisch91@gmail.com>
2020-04-26 17:02:45 +0200

    Full refactor

--- src/analyse_csv_data.py
+++ src/analyse_csv_data.py
@@ -1,18 +1,30 @@
+import sys
 import numpy as np
-import pandas as pd
 from pathlib import Path
 import argparse
+import datetime
+
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from datetime import datetime
 import seaborn as sns
 
-# Use seaborn style defaults and set the default figure size
-sns.set(rc={'figure.figsize':(11, 4)})
+# Constants
+MAIN_LIFTS = [
+        "Bench Press (Barbell)",
+        "Squat (Barbell)",
+        "Overhead press (Barbell)",
+        "Sumo Deadlift (Barbell)",
+        ]
 
-def calculate_1rm_epley(w, r):
-    """
-    Calculates estimated 1 RM using the Epley formula.
+
+# Use seaborn style defaults and set the default figure size
+sns.set(rc={"figure.figsize":(11, 4)})
+
+
+def calculate_1rm_epley(w: float, r: int):
+    """Calculates estimated 1 RM using the Epley formula
+
     weight w for r reps.
     """
 
@@ -23,34 +35,19 @@ def calculate_1rm_epley(w, r):
 
     return w*(1 + r/30)
 
-def get_est_1rm_by_date(data):
-    est_1rm_by_date = {}
-
-    for row in data.itertuples():
-        date = row.Date
-        if date in est_1rm_by_date:
-            est_1rm_by_date[date] = max(est_1rm_by_date[date],
-                                            calculate_1rm_epley(row.Weight, row.Reps))
-        else:
-            est_1rm_by_date[date] = calculate_1rm_epley(row.Weight, row.Reps)
-
-    return est_1rm_by_date
-
-def get_total_volume_by_date(data):
-    total_volume_by_date = {}
-
-    for row in data.itertuples():
-        date = row.Date
-        if date in total_volume_by_date:
-            total_volume_by_date[date] += row.Weight*row.Reps
-        else:
-            total_volume_by_date[date] = row.Weight*row.Reps
-
-    return total_volume_by_date
 
 def main():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("input_file", type=str, help="input data file")
+    parser.add_argument(
+            "--print-exercises", "-p", 
+            action="store_true", 
+            help="display all exercises in dataset")
+    parser.add_argument(
+            "--exercises", "-e", 
+            type=int,
+            nargs="+",
+            help="display all exercises in dataset")
     args = parser.parse_args()
 
     data_filepath = Path(args.input_file)
@@ -59,67 +56,45 @@ def main():
 
     df = pd.read_csv(data_filepath, sep=";")
     df["Date"] = pd.to_datetime(df["Date"])
+    unique_exercises = list(df["Exercise Name"].value_counts().index)
 
-    df_all_dates = pd.date_range(min(df["Date"]), max(df["Date"]), freq="D")
-    all_exercises = df["Exercise Name"].unique()
+    if args.print_exercises:
+        for i,ex in enumerate(unique_exercises):
+            print(f"{i}: {ex}")
+        sys.exit()
 
-    column_names = []
-    for exercise_name in all_exercises:
-        column_names.append("Est. 1 RM - " + exercise_name)
-        column_names.append("Total Volume - " + exercise_name)
+    if not args.exercises:
+        exercises = MAIN_LIFTS
+    else:
+        exercises = [unique_exercises[i] for i in args.exercises]
 
-    df_all_exercises = pd.DataFrame(columns=column_names)
-    df_all_exercises = df_all_exercises.assign(Date=df_all_dates)
+    for ex in exercises:
+        df_exercise = df.loc[df["Exercise Name"] == ex]
+        df_exercise.set_index("Date", inplace=True)
+        df_exercise.loc[:, "Set Volume"] = df_exercise["Weight"]*df_exercise["Reps"]
 
-    df_dict = {
-            "Overhead Press": df.loc[df["Exercise Name"] == "Overhead Press (Barbell)"],
-            "Squat": df.loc[df["Exercise Name"] == "Squat (Barbell)"],
-            "Sumo Deadlift": df.loc[df["Exercise Name"] == "Sumo Deadlift (Barbell)"],
-            "Bench Press": df.loc[df["Exercise Name"] == "Bench Press (Barbell)"],
-    }
-    new_df_dict = {}
+        grouper1 = df_exercise.groupby("Date")
+        d1 = grouper1["Set Volume"].sum().to_frame(name="Total Volume").reset_index()
 
-    for key, val in df_dict.items():
-        est_1rm_by_date = get_est_1rm_by_date(val)
-        total_volume_by_date = get_total_volume_by_date(val)
-        new_df_dict[key] = pd.DataFrame({
-                    "Date": [d for d in est_1rm_by_date],
-                    "Est. 1 RM": [est_1rm_by_date[d] for d in est_1rm_by_date],
-                    "Total Volume": [total_volume_by_date[d] for d in total_volume_by_date],
-                })
+        df_exercise["Est. 1 RM"] = df_exercise.apply(
+                lambda x: calculate_1rm_epley(x["Weight"], x["Reps"]), axis=1)
+        grouper2 = df_exercise.groupby("Date")
+        d2 = grouper2["Est. 1 RM"].max().to_frame(
+                name="Est. 1 RM").reset_index()
 
-    print(df_all_exercises)
+        d2["Total Volume"] = d1["Total Volume"]
 
-    data_columns_est_1rm = ["Est. 1 RM Squat", 
-					"Est. 1 RM Sumo Deadlift", 
-					"Est. 1 RM Overhead Press", 
-					"Est. 1 RM Bench Press"]
-
-    data_columns_volume = ["Total Volume Squat", 
-						   "Total Volume Sumo Deadlift", 
-					       "Total Volume Overhead Press", 
-					       "Total Volume Bench Press"]
-
-    for key, val in df_dict.items():
-        est_1rm_by_date = get_est_1rm_by_date(val)
-        total_volume_by_date = get_total_volume_by_date(val)
-        new_df_dict[key] = pd.DataFrame({
-                    "Date": [d for d in est_1rm_by_date],
-                    "Est. 1 RM": [est_1rm_by_date[d] for d in est_1rm_by_date],
-                    "Total Volume": [total_volume_by_date[d] for d in total_volume_by_date],
-                })
-
-
-    merged_df = new_df_dict["Squat"].merge(new_df_dict["Sumo Deadlift"], on="Date", how="outer", suffixes=(" Squat", " Sumo Deadlift")).merge(new_df_dict["Bench Press"], on="Date", how="outer").merge(new_df_dict["Overhead Press"], on="Date", how="outer", suffixes=(" Bench Press", " Overhead Press"))
-
-    workouts_daily = merged_df.sort_values(by="Date")
-    workouts_daily = workouts_daily.set_index("Date")
-
-    axes = workouts_daily[data_columns_est_1rm].plot(marker='.', linewidth=0.5, alpha=0.5, linestyle='None', figsize=(11, 9), subplots=True)
-    plt.savefig(out_folder / ("Est_1rm_" + data_filepath.with_suffix(".png").name))
-
-    axes = workouts_daily[data_columns_volume].plot(marker='.', linewidth=0.5, alpha=0.5, linestyle='None', figsize=(11, 9), subplots=True)
-    plt.savefig(out_folder / ("Tot_vol_" + data_filepath.with_suffix(".png").name))
+        axes = d2[["Est. 1 RM", "Total Volume"]].plot(
+                title=ex,
+                marker=".", 
+                linewidth=0.5, 
+                alpha=0.5, 
+                linestyle="None", 
+                figsize=(11, 9), 
+                subplots=True)
+        plt.savefig(
+                out_folder / f"{ex}_{data_filepath.with_suffix('.png').name}")
+        plt.show()
 	
 
 if __name__ == "__main__":
